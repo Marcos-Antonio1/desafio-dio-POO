66e161cb275909ce07b7a0658497d6c4015dfa61
Marcos-Antonio1 <marcos_m10antonio@outlook.com>
2022-02-28 19:15:04 -0300

    Mentoria e Curso

---
+++ src/br/com/dio/Main.java
@@ -0,0 +1,28 @@
+package br.com.dio;
+import br.com.dio.Dominio.Curso;
+import br.com.dio.Dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição do curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JavaScript");
+        curso2.setDescricao("Descrição javaScript Curso");
+
+        Mentoria mentoria1= new Mentoria();
+        mentoria1.setTitulo("mentoria Java");
+        mentoria1.setDescricao("Primeira mentoria de java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+
+    }
+}
